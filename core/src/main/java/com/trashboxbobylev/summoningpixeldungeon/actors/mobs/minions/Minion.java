c3aa4d886e78aed13ee84e5b3dd8854e5cc6a93a
TrashboxBobylev <trashbox.bobylev@gmail.com>
2019-09-06 17:55:31 +0700

    Implemented basic minion behaviour

--- core/src/main/java/com/trashboxbobylev/summoningpixeldungeon/actors/mobs/minions/Minion.java
+++ core/src/main/java/com/trashboxbobylev/summoningpixeldungeon/actors/mobs/minions/Minion.java
@@ -24,15 +24,108 @@
 
 package com.trashboxbobylev.summoningpixeldungeon.actors.mobs.minions;
 
+import com.trashboxbobylev.summoningpixeldungeon.Dungeon;
+import com.trashboxbobylev.summoningpixeldungeon.actors.Char;
 import com.trashboxbobylev.summoningpixeldungeon.actors.mobs.Mob;
+import com.watabou.utils.Random;
 
 public class Minion extends Mob {
+
+    protected int minDamage = 0;
+    protected int maxDamage = 0;
+    protected int minDR = 0;
+    protected int maxDR = 0;
+
     {
-        //all minions are kinda intelligent
+        //all minions are allies and kinda intelligent
+        alignment = Alignment.ALLY;
         intelligentAlly = true;
+
+        WANDERING = new Wandering();
+
+        //before other mobs
+        actPriority = MOB_PRIO + 1;
     }
 
-    public void setHP(int hp){
+    public void setMaxHP(int hp){
+        HP = HT = hp;
+    }
+
+    public void setDamage(int min, int max){
+        minDamage = min;
+        maxDamage = max;
+    }
+
+    public void adjustDamage(int min, int max){
+        minDamage += min;
+        maxDamage += max;
+    }
+
+    @Override
+    public int damageRoll() {
+        return Random.NormalIntRange(minDamage, maxDamage);
+    }
+
+    public void setDR(int min, int max){
+        minDR = min;
+        maxDR = max;
+    }
+
+    public void adjustDR(int min, int max){
+        minDR += min;
+        maxDR += max;
+    }
+
+    @Override
+    public int drRoll() {
+        return Random.NormalIntRange(minDR, maxDR);
+    }
+
+    @Override
+    protected Char chooseEnemy() {
+        Char enemy = super.chooseEnemy();
+
+        int targetPos = Dungeon.hero.pos;
+
+        //will never attack something far from their target
+        if (enemy != null
+                && Dungeon.level.mobs.contains(enemy)
+                && (Dungeon.level.distance(enemy.pos, targetPos) <= 8)){
+            return enemy;
+        }
+
+        return null;
+    }
+
+    //ported from DriedRose.java
+    //minions will always move towards hero if enemies not here
+    private class Wandering extends Mob.Wandering {
+
+        @Override
+        public boolean act( boolean enemyInFOV, boolean justAlerted ) {
+            if ( enemyInFOV) {
+                enemySeen = true;
+                notice();
+                alerted = true;
+                state = HUNTING;
+                target = enemy.pos;
+            } else {
+                enemySeen = false;
+
+                int oldPos = pos;
+                target = Dungeon.hero.pos;
+                //always move towards the hero when wandering
+                if (getCloser( target )) {
+                    spend( 1 / speed() );
+                    return moveSprite( oldPos, pos );
+                } else {
+                    spend( TICK );
+                }
+
+            }
+            return true;
+        }
 
     }
+
 }
