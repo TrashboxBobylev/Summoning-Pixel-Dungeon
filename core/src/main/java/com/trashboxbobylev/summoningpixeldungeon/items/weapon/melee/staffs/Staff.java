2b1a48008b9887b828061615c767829ba3b0e3ba
TrashboxBobylev <trashbox.bobylev@gmail.com>
2019-09-09 21:39:46 +0700

    Edited staff's recharge and some strings

--- core/src/main/java/com/trashboxbobylev/summoningpixeldungeon/items/weapon/melee/staffs/Staff.java
+++ core/src/main/java/com/trashboxbobylev/summoningpixeldungeon/items/weapon/melee/staffs/Staff.java
@@ -460,7 +460,7 @@ public class Staff extends MeleeWeapon {
 
     public class Charger extends Buff {
 
-        private static final float BASE_CHARGE_DELAY = 80f;
+        private static final float BASE_CHARGE_DELAY = 60f;
         private static final float SCALING_CHARGE_ADDITION = 28f;
         private static final float NORMAL_SCALE_FACTOR = 0.75f;
 
