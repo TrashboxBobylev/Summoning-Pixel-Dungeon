d4d2790fbf25cc0ab9c2eb5d6fd3c5dd13df5cfe
TrashboxBobylev <trashbox.bobylev@gmail.com>
2019-09-11 21:42:41 +0700

    Because of staffs, gold's rarity was increased, this commit compensates that change

--- core/src/main/java/com/trashboxbobylev/summoningpixeldungeon/items/Gold.java
+++ core/src/main/java/com/trashboxbobylev/summoningpixeldungeon/items/Gold.java
@@ -93,7 +93,7 @@ public class Gold extends Item {
 	
 	@Override
 	public Item random() {
-		quantity = Random.Int( 30 + Dungeon.depth * 10, 60 + Dungeon.depth * 20 );
+		quantity = Random.Int( 50 + Dungeon.depth * 12, 80 + Dungeon.depth * 25 );
 		return this;
 	}
 	
