4eb205bc27d2d3d4d480b20c2969a74b66597b1c
TrashboxBobylev <trashbox.bobylev@gmail.com>
2019-10-21 22:01:07 +0700

    Ech x7

--- core/src/main/java/com/trashboxbobylev/summoningpixeldungeon/items/spells/Contain.java
+++ core/src/main/java/com/trashboxbobylev/summoningpixeldungeon/items/spells/Contain.java
@@ -77,6 +77,7 @@ public class Contain extends TargetedSpell {
             ScrollOfTeleportation.appear(mb, bolt.collisionPos);
             Dungeon.level.occupyCell(mb);
             Buff.affect(mb, Amok.class, 15);
+            mb.next();
             for (int i= 0; i < 5; i++) Sample.INSTANCE.play(Assets.SND_MIMIC);
         } else {
             GLog.i(Messages.get(CursedWand.class, "nothing"));
