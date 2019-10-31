3c5c6f2aba98048d80b10e6f490a01a20e130467
TrashboxBobylev <trashbox.bobylev@gmail.com>
2019-10-31 18:56:40 +0600

    Going to release! Removal of other langs, fixes from Inevielle

--- core/src/main/java/com/trashboxbobylev/summoningpixeldungeon/SPDSettings.java
+++ core/src/main/java/com/trashboxbobylev/summoningpixeldungeon/SPDSettings.java
@@ -263,7 +263,7 @@ public class SPDSettings extends GameSettings {
 	
 	public static boolean systemFont(){
 		return getBoolean(KEY_SYSTEMFONT,
-				(language() == Languages.KOREAN || language() == Languages.CHINESE));
+				(language() == Languages.CHINESE));
 	}
 	
 }
