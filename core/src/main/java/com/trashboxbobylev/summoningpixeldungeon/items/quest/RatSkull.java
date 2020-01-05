a2192a0761b1f02abacbb87b1b688f28c601f81a
TrashboxBobylev <trashbox.bobylev@gmail.com>
2020-01-05 20:15:43 +0600

    1.1: what a heck i am doing?

--- core/src/main/java/com/trashboxbobylev/summoningpixeldungeon/items/quest/RatSkull.java
+++ core/src/main/java/com/trashboxbobylev/summoningpixeldungeon/items/quest/RatSkull.java
@@ -24,18 +24,67 @@
 
 package com.trashboxbobylev.summoningpixeldungeon.items.quest;
 
+import com.trashboxbobylev.summoningpixeldungeon.Assets;
+import com.trashboxbobylev.summoningpixeldungeon.Dungeon;
+import com.trashboxbobylev.summoningpixeldungeon.actors.Actor;
+import com.trashboxbobylev.summoningpixeldungeon.actors.Char;
+import com.trashboxbobylev.summoningpixeldungeon.actors.buffs.*;
+import com.trashboxbobylev.summoningpixeldungeon.actors.hero.Hero;
+import com.trashboxbobylev.summoningpixeldungeon.actors.mobs.Mob;
+import com.trashboxbobylev.summoningpixeldungeon.actors.mobs.minions.Minion;
+import com.trashboxbobylev.summoningpixeldungeon.effects.Beam;
+import com.trashboxbobylev.summoningpixeldungeon.items.EquipableItem;
 import com.trashboxbobylev.summoningpixeldungeon.items.Item;
+import com.trashboxbobylev.summoningpixeldungeon.items.KindofMisc;
+import com.trashboxbobylev.summoningpixeldungeon.items.Recipe;
+import com.trashboxbobylev.summoningpixeldungeon.items.artifacts.Artifact;
+import com.trashboxbobylev.summoningpixeldungeon.items.wands.Wand;
+import com.trashboxbobylev.summoningpixeldungeon.items.weapon.melee.staffs.Staff;
+import com.trashboxbobylev.summoningpixeldungeon.mechanics.Ballistica;
+import com.trashboxbobylev.summoningpixeldungeon.messages.Messages;
+import com.trashboxbobylev.summoningpixeldungeon.scenes.CellSelector;
+import com.trashboxbobylev.summoningpixeldungeon.scenes.GameScene;
 import com.trashboxbobylev.summoningpixeldungeon.sprites.ItemSpriteSheet;
+import com.trashboxbobylev.summoningpixeldungeon.tiles.DungeonTilemap;
+import com.trashboxbobylev.summoningpixeldungeon.ui.QuickSlotButton;
+import com.trashboxbobylev.summoningpixeldungeon.utils.GLog;
+import com.watabou.noosa.audio.Sample;
+import com.watabou.utils.Random;
 
-//this one's still hanging around to support quests from old saves
-//I may reuse it at some point.
-public class RatSkull extends Item {
+import java.util.ArrayList;
+
+//huh?
+public class RatSkull extends Artifact {
 	
 	{
 		image = ItemSpriteSheet.SKULL;
-		
+
+		charge = 0;
 		unique = true;
+		chargeCap = 100;
 	}
+
+	public static final String AC_USE = "USE";
+
+    @Override
+    public ArrayList<String> actions( Hero hero ) {
+        ArrayList<String> actions = super.actions( hero );
+        if (isEquipped( hero ) && charge == 100 && !cursed)
+            actions.add(AC_USE);
+        return actions;
+    }
+
+    @Override
+    public void execute( Hero hero, String action ) {
+
+        super.execute(hero, action);
+
+        if (action.equals(AC_USE)) {
+            curUser = hero;
+            curItem = this;
+            GameScene.selectCell( zapper );
+        }
+    }
 	
 	@Override
 	public boolean isUpgradable() {
@@ -46,4 +95,83 @@ public class RatSkull extends Item {
 	public boolean isIdentified() {
 		return true;
 	}
+
+    @Override
+    protected ArtifactBuff passiveBuff() {
+        return new skullRecharge();
+    }
+
+    public class skullRecharge extends ArtifactBuff{
+        @Override
+        public boolean act() {
+            LockedFloor lock = target.buff(LockedFloor.class);
+            if (charge < chargeCap && !cursed && (lock == null || lock.regenOn())) {
+                partialCharge += 1 / (50f - (chargeCap - charge)*5f);
+
+                if (partialCharge >= 1) {
+                    partialCharge --;
+                    charge ++;
+
+                    if (charge == chargeCap){
+                        partialCharge = 0;
+                    }
+                }
+            }
+
+            updateQuickslot();
+
+            spend( TICK );
+
+            return true;
+        }
+    }
+
+    protected static CellSelector.Listener zapper = new  CellSelector.Listener() {
+
+        @Override
+        public void onSelect( Integer target ) {
+
+            if (target != null) {
+
+                final Ballistica shot = new Ballistica( curUser.pos, target, Ballistica.MAGIC_BOLT);
+                int cell = shot.collisionPos;
+
+                if (target == curUser.pos || cell == curUser.pos) {
+                    GLog.i( Messages.get(Wand.class, "self_target") );
+                    return;
+                }
+
+                curUser.sprite.zap(cell);
+
+                //attempts to target the cell aimed at if something is there, otherwise targets the collision pos.
+                if (Actor.findChar(target) != null)
+                    QuickSlotButton.target(Actor.findChar(target));
+                else
+                    QuickSlotButton.target(Actor.findChar(cell));
+
+                    curUser.busy();
+                    Invisibility.dispel();
+
+                        curUser.sprite.parent.add(
+                                new Beam.LightRay(curUser.sprite.center(), DungeonTilemap.raisedTileCenterToWorld(shot.collisionPos)));
+                        Sample.INSTANCE.play( Assets.SND_ZAP );
+                        Char ch = Actor.findChar(shot.collisionPos);
+                        if (ch != null){
+                            ch.damage(Random.NormalIntRange(Dungeon.depth / 3, Math.round(Dungeon.depth * 1.5f)), this);
+                            if (!ch.isAlive() && ch.alignment == Char.Alignment.ENEMY){
+                                Buff.affect(curUser, Recharging.class, ((Mob)ch).EXP*2);
+                                Buff.affect(curUser, Bless.class, 3f);
+                                ((RatSkull)curItem).charge = 0;
+                                updateQuickslot();
+                            }
+                        }
+                    }
+                    curItem.cursedKnown = true;
+                }
+
+        @Override
+        public String prompt() {
+            return Messages.get(Wand.class, "prompt");
+        }
+    };
 }
