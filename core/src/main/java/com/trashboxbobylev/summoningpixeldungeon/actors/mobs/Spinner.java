5de2a3b0ae251d5fa177031611daf81d28d23380
TrashboxBobylev <trashbox.bobylev@gmail.com>
2020-01-21 21:46:01 +0600

    1.1: rewrited spinner's attacking

--- core/src/main/java/com/trashboxbobylev/summoningpixeldungeon/actors/mobs/Spinner.java
+++ core/src/main/java/com/trashboxbobylev/summoningpixeldungeon/actors/mobs/Spinner.java
@@ -26,6 +26,7 @@ package com.trashboxbobylev.summoningpixeldungeon.actors.mobs;
 
 import com.trashboxbobylev.summoningpixeldungeon.Assets;
 import com.trashboxbobylev.summoningpixeldungeon.Dungeon;
+import com.trashboxbobylev.summoningpixeldungeon.actors.Actor;
 import com.trashboxbobylev.summoningpixeldungeon.actors.Char;
 import com.trashboxbobylev.summoningpixeldungeon.actors.blobs.Blob;
 import com.trashboxbobylev.summoningpixeldungeon.actors.blobs.Web;
@@ -39,6 +40,7 @@ import com.trashboxbobylev.summoningpixeldungeon.effects.Splash;
 import com.trashboxbobylev.summoningpixeldungeon.effects.WhiteWound;
 import com.trashboxbobylev.summoningpixeldungeon.items.food.MysteryMeat;
 import com.trashboxbobylev.summoningpixeldungeon.mechanics.Ballistica;
+import com.trashboxbobylev.summoningpixeldungeon.messages.Messages;
 import com.trashboxbobylev.summoningpixeldungeon.scenes.GameScene;
 import com.trashboxbobylev.summoningpixeldungeon.sprites.CharSprite;
 import com.trashboxbobylev.summoningpixeldungeon.sprites.SpinnerSprite;
@@ -62,6 +64,7 @@ public class Spinner extends Mob {
 		lootChance = 0.125f;
 
 		FLEEING = new Fleeing();
+		HUNTING = new Hunting();
 	}
 
 	@Override
@@ -81,9 +84,8 @@ public class Spinner extends Mob {
 
 	@Override
 	protected boolean act() {
-		boolean result = super.act();
         if (buff(RoseWraith.Timer.class) != null) sprite.showStatus(CharSprite.DEFAULT, String.valueOf(Math.round(buff(RoseWraith.Timer.class).cooldown()+1)));
-        if (enemy !=null && state == HUNTING && buff(RoseWraith.Timer.class) == null && enemySeen) {
+        if (enemy != null && state == HUNTING && buff(RoseWraith.Timer.class) == null && enemySeen) {
             final Ballistica ballistica = new Ballistica(pos, enemy.pos, Ballistica.PROJECTILE);
             final Char spinner = this;
             if (Dungeon.level.heroFOV[pos]) sprite.zap(enemy.pos);
@@ -101,11 +103,12 @@ public class Spinner extends Mob {
                                 CellEmitter.get(cell + i).burst(MagicMissile.ForceParticle.FACTORY, 15);
                             }
                             Buff.affect(spinner, RoseWraith.Timer.class, 10f);
+                            return;
                         }
                     } );
             Sample.INSTANCE.play(Assets.SND_BADGE);
         }
-		return result;
+		return super.act();
 	}
 
     @Override
@@ -156,4 +159,68 @@ public class Spinner extends Mob {
 			}
 		}
 	}
+
+	private boolean trap(int target){
+	    if (enemy.properties().contains(Property.IMMOVABLE) || buff (RoseWraith.Timer.class) != null) return false;
+
+	    final Ballistica path = new Ballistica(pos, target, Ballistica.PROJECTILE);
+	    if (path.collisionPos != enemy.pos) return false;
+	    else {
+            int newPos = -1;
+            for (int i : path.subPath(1, path.dist)){
+                if (!Dungeon.level.solid[i] && Actor.findChar(i) == null){
+                    newPos = i;
+                    break;
+                }
+            }
+
+            if (newPos == -1){
+                return false;
+            } else {
+                final int newPosFinal = newPos;
+                this.target = newPos;
+                yell( Messages.get(this, "scorpion") );
+                final Char spinner = this;
+                if (Dungeon.level.heroFOV[pos]) sprite.zap(enemy.pos);
+                MagicMissile.boltFromChar( sprite.parent,
+                        MagicMissile.SPINNER,
+                        sprite,
+                        enemy.pos,
+                        new Callback() {
+                            @Override
+                            public void call() {
+                                int cell = path.collisionPos;
+                                Sample.INSTANCE.play(Assets.SND_PUFF);
+                                for (int i : PathFinder.NEIGHBOURS8) {
+                                    if (Dungeon.level.passable[cell + i]) GameScene.add(Blob.seed(cell + i, Random.Int(5, 10), Web.class));
+                                    CellEmitter.get(cell + i).burst(MagicMissile.ForceParticle.FACTORY, 15);
+                                }
+                                Dungeon.hero.interrupt();
+
+                            }
+                        } );
+                Sample.INSTANCE.play(Assets.SND_BADGE);
+                next();
+            }
+        }
+        Buff.affect(this, RoseWraith.Timer.class, 10f);
+	    return true;
+    }
+
+    private class Hunting extends Mob.Hunting{
+        @Override
+        public boolean act( boolean enemyInFOV, boolean justAlerted ) {
+            enemySeen = enemyInFOV;
+
+            if (buff(RoseWraith.Timer.class) == null
+                    && enemyInFOV
+                    && !isCharmedBy( enemy )
+                    && trap(enemy.pos)){
+                return false;
+            } else {
+                return super.act( enemyInFOV, justAlerted );
+            }
+
+        }
+    }
 }
