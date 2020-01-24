87e3106b9b37b629e8536f38191f2dc197dc9d64
TrashboxBobylev <trashbox.bobylev@gmail.com>
2020-01-24 16:24:00 +0600

    Debug: testing explosions

--- core/src/main/java/com/trashboxbobylev/summoningpixeldungeon/actors/hero/HeroClass.java
+++ core/src/main/java/com/trashboxbobylev/summoningpixeldungeon/actors/hero/HeroClass.java
@@ -139,12 +139,7 @@ public enum HeroClass {
 		new WoollyBomb().identify().collect();
 		new Noisemaker().identify().collect();
 		new ArcaneBomb().identify().collect();
-        AlchemistsToolkit a = new AlchemistsToolkit();
-        a.charge = 100;
-        a.identify();
-        a.collect();
-        new ScrollOfRemoveCurse().identify().collect();
-        new Bomb().collect();
+        new Bomb().quantity(25).collect();
 
 		new PotionOfExperience().quantity(18).collect();
 
