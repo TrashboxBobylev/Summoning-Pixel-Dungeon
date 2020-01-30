72e23a913dff84b58a789583ad4846be6e01ecf6
TrashboxBobylev <trashbox.bobylev@gmail.com>
2020-01-30 21:19:39 +0600

    1.1: heavy arrow is slower (hacky way, but idk)

--- core/src/main/java/com/trashboxbobylev/summoningpixeldungeon/sprites/MissileSprite.java
+++ core/src/main/java/com/trashboxbobylev/summoningpixeldungeon/sprites/MissileSprite.java
@@ -143,6 +143,9 @@ public class MissileSprite extends ItemSprite implements Tweener.Listener {
 		}
 		
 		float speed = SPEED;
+		if (item instanceof SpiritBow.SpiritArrow && item.image() == ItemSpriteSheet.SPIRIT_BLAST){
+		    speed *= 0.4f;
+        }
 		if (item instanceof Dart && Dungeon.hero.belongings.weapon instanceof Crossbow){
 			speed *= 3f;
 			
