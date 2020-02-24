846b412a3eee8ac24a36a8e10c8a45baffa5cd21
TrashboxBobylev <trashbox.bobylev@gmail.com>
2020-02-24 21:37:54 +0600

    1.1.3: added safety check to soul wielder's buff

--- core/src/main/java/com/trashboxbobylev/summoningpixeldungeon/actors/buffs/powers/SoulReaver.java
+++ core/src/main/java/com/trashboxbobylev/summoningpixeldungeon/actors/buffs/powers/SoulReaver.java
@@ -49,7 +49,7 @@ public class SoulReaver extends DummyBuff {
     @Override
     public boolean attachTo(Char target) {
         if (super.attachTo( target )) {
-            target.sprite.emitter().burst(WhiteParticle.UP, 8);
+            if (target != null) target.sprite.emitter().burst(WhiteParticle.UP, 8);
             return true;
         } else {
             return false;
