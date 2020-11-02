abd6e70d8ff5c2fde6c38872b48ca6de90abcb77
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2020-11-02 22:26:11 +0300

    1.1.5: changelog

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ui/changelist/SummPDChanges.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ui/changelist/SummPDChanges.java
@@ -54,6 +54,7 @@ public class SummPDChanges {
 		//ChangeInfo changes = new ChangeInfo( "1.1", true, "");
 		//changes.hardlight( Window.TITLE_COLOR);
 		//changeInfos.add(changes);
+        add_1_1_5_Changes(changeInfos);
         add_1_1_4_Changes(changeInfos);
         add_1_1_3_Changes(changeInfos);
         add_1_1_2_Changes(changeInfos);
