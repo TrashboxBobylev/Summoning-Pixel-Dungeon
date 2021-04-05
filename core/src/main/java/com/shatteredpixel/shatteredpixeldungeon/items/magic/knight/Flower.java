db116f2a10170f82ccbea425024921bb290219c1
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2021-04-05 22:53:04 +0300

    1.1.7: implemented flowers

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/magic/knight/Flower.java
@@ -0,0 +1,85 @@
+/*
+ * Pixel Dungeon
+ *   * Copyright (C) 2012-2015 Oleg Dolya
+ *   *
+ *   * Shattered Pixel Dungeon
+ *   * Copyright (C) 2014-2019 Evan Debenham
+ *   *
+ *   * Summoning Pixel Dungeon
+ *   * Copyright (C) 2019-2020 TrashboxBobylev
+ *   *
+ *   * This program is free software: you can redistribute it and/or modify
+ *   * it under the terms of the GNU General Public License as published by
+ *   * the Free Software Foundation, either version 3 of the License, or
+ *   * (at your option) any later version.
+ *   *
+ *   * This program is distributed in the hope that it will be useful,
+ *   * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   * GNU General Public License for more details.
+ *   *
+ *   * You should have received a copy of the GNU General Public License
+ *   * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.shatteredpixel.shatteredpixeldungeon.items.magic.knight;
+
+import com.shatteredpixel.shatteredpixeldungeon.Challenges;
+import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.powers.FlowersCD;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.HeroSubClass;
+import com.shatteredpixel.shatteredpixeldungeon.effects.CellEmitter;
+import com.shatteredpixel.shatteredpixeldungeon.effects.particles.LeafParticle;
+import com.shatteredpixel.shatteredpixeldungeon.items.magic.ConjurerSpell;
+import com.shatteredpixel.shatteredpixeldungeon.levels.Level;
+import com.shatteredpixel.shatteredpixeldungeon.levels.Terrain;
+import com.shatteredpixel.shatteredpixeldungeon.mechanics.Ballistica;
+import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
+import com.shatteredpixel.shatteredpixeldungeon.plants.Plant;
+import com.shatteredpixel.shatteredpixeldungeon.plants.Swiftthistle;
+import com.shatteredpixel.shatteredpixeldungeon.scenes.GameScene;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
+import com.watabou.utils.PathFinder;
+
+import java.text.DecimalFormat;
+
+public class Flower extends ConjurerSpell {
+
+    {
+        image = ItemSpriteSheet.FLOWER;
+    }
+
+    @Override
+    public void effect(Ballistica trajectory) {
+        int pos = trajectory.collisionPos;
+        if ((Dungeon.level.map[pos] != Terrain.ALCHEMY
+                && !Dungeon.level.pit[pos]
+                && Dungeon.level.traps.get(pos) == null
+                && !Dungeon.isChallenged(Challenges.NO_HERBALISM)))
+        Dungeon.level.plant(new Swiftthistle.Seed(), pos);
+        Buff.affect(Dungeon.hero, FlowersCD.class, cooldown());
+    }
+
+    private int cooldown(){
+        switch (level()){
+            case 1: return 20;
+            case 2: return 10;
+        }
+        return 40;
+    }
+
+    @Override
+    public int manaCost() {
+        switch (level()){
+            case 1: return 36;
+            case 2: return 72;
+        }
+        return 18;
+    }
+
+    public String desc() {
+        return Messages.get(this, "desc", cooldown(), manaCost());
+    }
+
+}
