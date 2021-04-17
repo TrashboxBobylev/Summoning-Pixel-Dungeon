ee7a4ad9f2bc07249671494663e7d80e3f3bcc53
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2021-04-17 13:06:22 +0300

    1.1.7: fixed glitch on tengu

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/NewPrisonBossLevel.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/NewPrisonBossLevel.java
@@ -126,6 +126,20 @@ public class NewPrisonBossLevel extends Level {
 		for (Bundlable item : bundle.getCollection(STORED_ITEMS)){
 			storedItems.add( (Item)item );
 		}
+
+		//if for some reason we don't have needed key, give key
+		boolean key = false;
+		for (Heap heap : heaps.valueList()){
+			for (Item item: itemsToSpawn.toArray(new Item[0])){
+				if (item instanceof IronKey){
+					key = true;
+				}
+			}
+		}
+
+		if (!key){
+			drop(new IronKey(Dungeon.chapterSize()*2), Dungeon.hero.pos);
+		}
 		
 		triggered = bundle.getBooleanArray(TRIGGERED);
 		
@@ -527,7 +541,7 @@ public class NewPrisonBossLevel extends Level {
 		if (item != null) {
 			drop( item, randomRespawnCell( null ) ).setHauntedIfCursed().type = Heap.Type.REMAINS;
 		}
-		drop(new IronKey(10), randomPrisonCellPos());
+		drop(new IronKey(Dungeon.chapterSize()*2), randomPrisonCellPos());
 	}
 	
 	private int randomPrisonCellPos(){
