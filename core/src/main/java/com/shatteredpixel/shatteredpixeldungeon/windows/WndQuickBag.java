171a2b54d56b273f6de786e602aca081b0b01919
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2021-07-09 19:23:45 +0300

    1.2.1: adjusted quick bag to account for dynamic actions

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/windows/WndQuickBag.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/windows/WndQuickBag.java
@@ -72,7 +72,7 @@ public class WndQuickBag extends Window {
 		ArrayList<Item> items = new ArrayList<>();
 
 		for (Item i : bag == null ? Dungeon.hero.belongings : bag){
-			if (i.defaultAction == null){
+			if (i.getDefaultAction() == null){
 				continue;
 			}
 			if (i instanceof Bag) {
