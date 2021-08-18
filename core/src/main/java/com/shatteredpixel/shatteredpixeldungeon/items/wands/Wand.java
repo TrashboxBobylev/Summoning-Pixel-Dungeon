15088bde7e29388c5d6a6c38e12850c1f76ac824
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2021-08-19 00:50:15 +0300

    1.2.2: fixed emitter on non-equipped wands

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/wands/Wand.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/wands/Wand.java
@@ -460,7 +460,7 @@ public abstract class Wand extends Weapon {
 
 	@Override
 	public Emitter emitter() {
-		if (Dungeon.hero == null && !isEquipped(Dungeon.hero)) return null;
+		if (Dungeon.hero == null || !isEquipped(Dungeon.hero)) return null;
 		Emitter emitter = new Emitter();
 		emitter.pos(11.5f, 1.5f);
 		emitter.fillTarget = false;
