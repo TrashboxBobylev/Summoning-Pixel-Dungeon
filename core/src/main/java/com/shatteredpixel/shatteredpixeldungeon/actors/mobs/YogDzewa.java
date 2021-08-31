f4d3a7c1673230f694ce95e5114740055e5dae32
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2021-08-31 13:23:21 +0300

    1.2.2: fixed yog not advancing phases

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/YogDzewa.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/YogDzewa.java
@@ -384,6 +384,8 @@ public class YogDzewa extends Mob {
 
 		if (phase < 4 && HP <= HT - 300*phase){
 
+			phase++;
+
 			Dungeon.level.viewDistance = Math.max(1, Dungeon.level.viewDistance-1);
 			if (Dungeon.hero.buff(Light.class) == null){
 				Dungeon.hero.viewDistance = Dungeon.level.viewDistance;
