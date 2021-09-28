7d1c932726215c11a4027b9ed0ef04635b44254f
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2021-09-28 14:49:40 +0300

    1.2.2a: removed more leftover code from challenges

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/Challenges.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/Challenges.java
@@ -39,35 +39,6 @@ import com.shatteredpixel.shatteredpixeldungeon.plants.Sungrass;
 
 public class Challenges {
 
-	//Some of these internal IDs are outdated and don't represent what these challenges do
-	public static final int NO_FOOD				= 1;
-	public static final int NO_ARMOR			= 2;
-	public static final int NO_HEALING			= 4;
-	public static final int NO_HERBALISM		= 8;
-	public static final int SWARM_INTELLIGENCE	= 16;
-	public static final int DARKNESS			= 32;
-	public static final int NO_SCROLLS		    = 64;
-
-	public static final int MAX_VALUE           = 127;
-
-	public static final String[] NAME_IDS = {
-			"no_food",
-			"no_armor",
-			"no_healing",
-			"no_herbalism",
-			"swarm_intelligence",
-			"darkness",
-			"no_scrolls"
-	};
-
-	public static final int[] MASKS = {
-			NO_FOOD, NO_ARMOR, NO_HEALING, NO_HERBALISM, SWARM_INTELLIGENCE, DARKNESS, NO_SCROLLS
-	};
-
-	public static int activeChallenges(){
-		return 0;
-	}
-
 	public static boolean isItemBlocked( Item item ){
 
 		if (Dungeon.mode == Dungeon.GameMode.NO_SOU && item instanceof ScrollOfUpgrade){
