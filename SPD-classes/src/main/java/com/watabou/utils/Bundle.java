0b373fe5f1001d83cb127b93142e313a3d312437
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2021-09-28 20:29:42 +0300

    1.2.2a: added bundle support for GameSettings

--- SPD-classes/src/main/java/com/watabou/utils/Bundle.java
+++ SPD-classes/src/main/java/com/watabou/utils/Bundle.java
@@ -458,6 +458,17 @@ public class Bundle {
 		}
 	}
 
+	public static Bundle fromString(String value){
+		Object json = new JSONTokener( value ).nextValue();
+
+		//if the data is an array, put it in a fresh object with the default key
+		if (json instanceof JSONArray){
+			json = new JSONObject().put( DEFAULT_KEY, json );
+		}
+
+		return new Bundle( (JSONObject) json );
+	}
+
 	public static boolean write( Bundle bundle, OutputStream stream ){
 		return write(bundle, stream, compressByDefault);
 	}
