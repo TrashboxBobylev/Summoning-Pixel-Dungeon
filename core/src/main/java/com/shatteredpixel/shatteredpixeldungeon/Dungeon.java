5776b07ee5d2f9b14532c04bf7dd71a1f7e24559
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2021-10-04 00:50:04 +0300

    1.2.2f: trying to fix strength issue in small dungeons

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/Dungeon.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/Dungeon.java
@@ -465,7 +465,7 @@ public class Dungeon {
 
 			//pos drops every two floors, (numbers 1-2, and 3-4) with a 50% chance for the earlier one each time.
 			int targetPOSLeft = Dungeon.chapterSize() / 2 - floorThisSet/2;
-			if (floorThisSet % (Dungeon.chapterSize() / 2) == 1 && Random.Int(2) == 0) targetPOSLeft --;
+			if (floorThisSet % (Dungeon.chapterSize() / 2) == 1 && (Random.Int(2) == 0 && Dungeon.chapterSize() == 4)) targetPOSLeft --;
 
 			return targetPOSLeft < posLeftThisSet;
 		}
