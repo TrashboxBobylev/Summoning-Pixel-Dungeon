d4ee402e51c6c403007d776f59e60bef94c4a73d
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2021-10-27 18:25:14 +0300

    1.2.2k: fixed outright stupid prize generation for prison pylon room

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/rooms/standard/GuardRoom.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/rooms/standard/GuardRoom.java
@@ -82,7 +82,7 @@ public class GuardRoom extends StandardRoom {
 			int pos;
 			do {
 				pos = level.pointToCell(random());
-			} while (level.distance(pos, guard.pos) == Terrain.EMPTY_SP || level.solid[pos]);
+			} while (level.map[pos] == Terrain.EMPTY_SP || level.solid[pos]);
 			level.drop( prize, pos );
 		}
 		int pos;
