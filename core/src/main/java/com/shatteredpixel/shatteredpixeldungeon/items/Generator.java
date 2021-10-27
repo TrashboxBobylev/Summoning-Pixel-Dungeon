41ec4a143ef143f70e2e27c014b571e8b1858d22
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2021-10-27 18:21:48 +0300

    1.2.2k: fixed the crash for transmuting t1

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Generator.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Generator.java
@@ -258,7 +258,7 @@ public class Generator {
 					Gloves.class,
 					Dagger.class
 			};
-			Category.WEP_T1.probs = new float[]{ 1, 1, 1, 0 };
+			Category.WEP_T1.probs = new float[]{ 1, 1, 1 };
 
 			Category.WEP_T2.classes = new Class<?>[]{
 					Shortsword.class,
