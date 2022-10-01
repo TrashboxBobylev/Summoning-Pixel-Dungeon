6b4207bb9be74efd361ca5766a3a4f84fa3c3cca
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2022-10-01 13:47:03 +0300

    1.2.3: tweaked Enlightened One:
    - zen sense only shows the tile with monsters themselves
    - removed amnesia effect

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/Dungeon.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/Dungeon.java
@@ -60,8 +60,6 @@ import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashSet;
 
-import static com.shatteredpixel.shatteredpixeldungeon.Conducts.Conduct.ZEN;
-
 public class Dungeon {
 
 	//enum of items which have limited spawns, records how many have spawned
@@ -808,13 +806,6 @@ public class Dungeon {
 		for (int i = t; i <= b; i++) {
 			BArray.or( level.visited, level.heroFOV, pos, width, level.visited );
 			pos+=level.width();
-		}
-		if (Dungeon.isChallenged(ZEN)) {
-
-			if (level.needUpdateFog!=null) {
-				level.needUpdateFog = BArray.and(BArray.not(level.heroFOV,null),BArray.or(level.visited, level.needUpdateFog, null),null);
-			} else level.needUpdateFog = BArray.and(level.visited,BArray.not(level.heroFOV,null),null);
-			level.visited = level.heroFOV;
 		}
 	
 		GameScene.updateFog(l, t, width, height);
