ccb09f4953cfe064723607a826fa7588bbcbe9d8
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2023-07-24 22:34:40 +0700

    1.2.5: fixed the ability to inscribe cloak without being actually able to inscribe cloak

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Stylus.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Stylus.java
@@ -29,8 +29,10 @@ import com.shatteredpixel.shatteredpixeldungeon.Conducts;
 import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Belongings;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Talent;
 import com.shatteredpixel.shatteredpixeldungeon.effects.Enchanting;
 import com.shatteredpixel.shatteredpixeldungeon.effects.particles.PurpleParticle;
+import com.shatteredpixel.shatteredpixeldungeon.items.artifacts.CloakOfShadows;
 import com.shatteredpixel.shatteredpixeldungeon.items.bags.Bag;
 import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
 import com.shatteredpixel.shatteredpixeldungeon.scenes.GameScene;
@@ -131,7 +133,10 @@ public class Stylus extends Item {
 
 		@Override
 		public boolean itemSelectable(Item item) {
-			return item instanceof Inscribable;
+			if (item instanceof Inscribable){
+				return !(item instanceof CloakOfShadows) || Dungeon.hero.hasTalent(Talent.ARCANE_CLOAK);
+			}
+			else return false;
 		}
 
 		@Override
